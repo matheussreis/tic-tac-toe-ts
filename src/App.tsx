8a804114362ca853e5f40838fd47fe5cb5b3be58
Matheus Reis <53496571+matheussreis@users.noreply.github.com>
2024-10-14 19:18:39 +0100

    setup app home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import GameBoard from './components/GameBoard';
+import ScoreBoard from './components/ScoreBoard';
+import HistoryPanel from './components/HistoryPanel';
+
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <main className="grid min-h-[50vh]">
+      <article className="flex flex-col p-4 gap-6 justify-center items-center">
+        <section className="bg-white rounded flex flex-col p-8 gap-6 justify-center items-center">
+          <h1 className="text-4xl font-bold select-none">Tic Tac Toe</h1>
+          <GameBoard />
+          <ScoreBoard />
+          <HistoryPanel />
+        </section>
+      </article>
+    </main>
+  );
 }
